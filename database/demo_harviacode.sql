1a7efda290dd48fdc816d82e9455849cfa5c70f2
Lutfi Haridha <lutfiharidha1337@gmail.com>
2019-07-31 16:35:02 +0700

    haha

--- database/demo_harviacode.sql
+++ database/demo_harviacode.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 17, 2017 at 06:06 PM
--- Server version: 10.1.9-MariaDB
--- PHP Version: 5.6.15
+-- Host: localhost
+-- Generation Time: Jul 31, 2019 at 11:31 AM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.2.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -30,30 +32,30 @@ CREATE TABLE `pangkat` (
   `id` int(11) NOT NULL,
   `nama` varchar(100) NOT NULL,
   `nip` varchar(20) NOT NULL,
-  `tgl_pangkat1` date NOT NULL,
-  `tgl_pangkat2` date NOT NULL
+  `jatuh_tempo` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `pangkat`
 --
 
-INSERT INTO `pangkat` (`id`, `nama`, `nip`, `tgl_pangkat1`, `tgl_pangkat2`) VALUES
-(1, 'Mas Admin', '198001022000021001', '2013-09-30', '2017-09-30'),
-(2, 'Mas Kuncung', '198001022000021002', '2013-10-15', '2017-10-15'),
-(3, 'Mas Sugeng', '198001022000021003', '2013-11-30', '2017-11-30');
+INSERT INTO `pangkat` (`id`, `nama`, `nip`, `jatuh_tempo`) VALUES
+(1, 'Mas Admin', '198001022000021001', '2019-09-30'),
+(2, 'Mas Kuncung', '198001022000021002', '2019-10-15'),
+(3, 'Mas Sugeng', '198001022000021003', '2019-08-05'),
+(4, 'yanis', '195410111980031004', '2019-08-07');
 
 -- --------------------------------------------------------
 
 --
 -- Stand-in structure for view `view_pangkat`
+-- (See below for the actual view)
 --
 CREATE TABLE `view_pangkat` (
 `id` int(11)
 ,`nama` varchar(100)
 ,`nip` varchar(20)
-,`tgl_pangkat1` date
-,`tgl_pangkat2` date
+,`jatuh_tempo` date
 ,`selisih` int(7)
 );
 
@@ -64,7 +66,7 @@ CREATE TABLE `view_pangkat` (
 --
 DROP TABLE IF EXISTS `view_pangkat`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_pangkat`  AS  select `pangkat`.`id` AS `id`,`pangkat`.`nama` AS `nama`,`pangkat`.`nip` AS `nip`,`pangkat`.`tgl_pangkat1` AS `tgl_pangkat1`,`pangkat`.`tgl_pangkat2` AS `tgl_pangkat2`,(to_days(`pangkat`.`tgl_pangkat2`) - to_days(now())) AS `selisih` from `pangkat` ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_pangkat`  AS  select `pangkat`.`id` AS `id`,`pangkat`.`nama` AS `nama`,`pangkat`.`nip` AS `nip`,`pangkat`.`jatuh_tempo` AS `jatuh_tempo`,to_days(`pangkat`.`jatuh_tempo`) - to_days(current_timestamp()) AS `selisih` from `pangkat` ;
 
 --
 -- Indexes for dumped tables
@@ -84,7 +86,9 @@ ALTER TABLE `pangkat`
 -- AUTO_INCREMENT for table `pangkat`
 --
 ALTER TABLE `pangkat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
